b4afec3e9f9b662c5f0ad0973e896d42c334fb9b
Knorr2710 <138547805+Knorr2710@users.noreply.github.com>
2024-03-28 08:14:03 +0700

    Add Games link to navigation menu and update language to Vietnamese

---
+++ script.js
@@ -0,0 +1,7 @@
+function togglePattern() {
+    if (document.querySelector("div#pattern").style.display == "block") {
+        document.querySelector("div#pattern").style.display = "none";
+    } else {
+        document.querySelector("div#pattern").style.display = "block"
+    }
+}
