5489226449f27c52569a9857b90e647eec10b636
Knorr2710 <138547805+Knorr2710@users.noreply.github.com>
2024-03-28 12:43:12 +0700

    Add toggle button and movie list functionality

---
+++ movie.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const list = document.querySelector("#movie-list ul");
+    const forms = document.forms;
+
+    // Delete movies
+    let deleteBtn = document.querySelectorAll(".delete")
+    list.addEventListener("click", function(event) {
+        if (event.target.className == "delete") {
+            const li = event.target.parentElement;
+            li.parentNode.removeChild(li);
+        }
+    })
+
+    // Add movies
+    const addForm = forms["add-movie"];
+    addForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const value = addForm.querySelector('input[type="text"]').value;
+
+        // Create element
+        const li = document.createElement("li");
+        const movieName = document.createElement("span");
+        const deleteBtn = document.createElement("span");
+
+        // Add text content
+        movieName.textContent = value;
+        deleteBtn.textContent = "delete";
+
+        // Add classes
+        movieName.classList.add("name");
+        deleteBtn.classList.add("delete");
+
+        // Append to DOM
+        li.appendChild(movieName);
+        li.appendChild(deleteBtn);
+        list.appendChild(li);
+    })
+})
